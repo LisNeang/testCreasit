9b45675ae6dc3a0fe0d5b0495baa784f511ea06e
Lis Neang <lisneang@gmail.com>
2022-03-12 10:30:30 +0100

    appel au click modifié pour loadMore - addEventListener au lieu de on clic

--- Desktop/testCreasit/js/articlesFunctions.js
+++ Desktop/testCreasit/js/articlesFunctions.js
@@ -1,58 +1,61 @@
+// ref https://www.w3schools.com/jsref/event_onload.asp
+
+
 //afficher au chargement que les 3 premiers articles
-const lItems = document.getElementsByClassName("urlArticle");
+    const lItems = document.getElementsByClassName("urlArticle");
+
+    function loadFirst3()
+    {
+        for(let i = 3; i < lItems.length; i++) {
+            lItems[i].style.display = "none";
+          }
+    }
 
 
-function loadFirst3()
- {
+    // ajouter addEventlistener
+    const afficherPlusBtn = document.getElementById("afficherPlusBtn");
 
-    for(let i = 3; i < lItems.length; i++) {
-        lItems[i].style.display = "none";
-      }
- }
+    afficherPlusBtn.addEventListener("click", loadMore);
 
-// afficher 3 résultats en + à chaque clic
- function loadMore()
- {
-    let countArticleAdded = 0;
-     
-    for(let i = 3; i < lItems.length; i++) {
-        if(lItems[i].style.display == "none" ){
-            lItems[i].style.display = "flex";
-            countArticleAdded = countArticleAdded+1;
-            console.log(i);
-            console.log(lItems.length-1);
-            if(i==(lItems.length-1)){
-                const afficherPlusBtn = document.getElementById("afficherPlusBtn");
-                console.log(afficherPlusBtn);
-                afficherPlusBtn.style.display = "none";
-    
-            }
-            if (countArticleAdded>2){
-                break;
-            }
+    // afficher 3 résultats en + à chaque clic
+    function loadMore()
+    {
+      console.log("load more fonct top");
+        let countArticleAdded = 0;
+        
+        for(let i = 3; i < lItems.length; i++) {
+            if(lItems[i].style.display == "none" ){
+                lItems[i].style.display = "flex";
+                countArticleAdded = countArticleAdded+1;
+                console.log(i);
+                console.log(lItems.length-1);
+                if(i==(lItems.length-1)){
+                    const afficherPlusBtn = document.getElementById("afficherPlusBtn");
+                    console.log(afficherPlusBtn);
+                    afficherPlusBtn.style.display = "none";
+        
+                }
+                if (countArticleAdded>2){
+                    break;
+                }
+            }   
+          }
+          textResultats();
+    }
+
+    // afficher le nombre de résultats
+    function textResultats()
+    {
+      const lItemsLength = lItems.length;
+      const textResultats = document.getElementsByClassName("texteResultats")[0];
+      
+      let countArticleAffiche = 3;
+      for(let i = 3; i < lItems.length; i++) {
+
+        if(lItems[i].style.display == "flex" ){
+          countArticleAffiche = countArticleAffiche+1;
         }   
       }
-      textResultats();
- }
-
- // afficher le nombre de résultats
- function textResultats()
- {
-  const lItemsLength = lItems.length;
-   const textResultats = document.getElementsByClassName("texteResultats")[0];
-   
-   let countArticleAffiche = 3;
-   for(let i = 3; i < lItems.length; i++) {
-
-    if(lItems[i].style.display == "flex" ){
-      countArticleAffiche = countArticleAffiche+1;
-    }   
-  }
-   textResultats.textContent='Vous avez vu ' + countArticleAffiche + ' résultats sur ' + lItemsLength;
- }
-
-
- 
-
-
-
+      textResultats.textContent='Vous avez vu ' + countArticleAffiche + ' résultats sur ' + lItemsLength;
+    }
+  
